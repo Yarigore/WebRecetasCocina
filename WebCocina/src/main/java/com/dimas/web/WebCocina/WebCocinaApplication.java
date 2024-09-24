14ea02a6f23cd8b80df68eb35790b879c9e9e819
Dimas <63927493+Yarigore@users.noreply.github.com>
2024-09-24 12:21:21 +0200

    Inicio proyecto Spring Boot

---
+++ WebCocina/src/main/java/com/dimas/web/WebCocina/WebCocinaApplication.java
@@ -0,0 +1,13 @@
+package com.dimas.web.WebCocina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCocinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCocinaApplication.class, args);
+	}
+
+}
